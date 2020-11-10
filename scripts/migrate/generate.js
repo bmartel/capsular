f9a4888a84ea2ce3213d9e477356ef6f53b12ddc
Brandon Martel <brandonmartel@gmail.com>
2020-11-10 17:38:37 -0600

    fix gen for js

--- scripts/migrate/generate.js
+++ scripts/migrate/generate.js
@@ -82,7 +82,7 @@ export const handler = function (argv) {
                     `
 import * as m_${version} from './${db}/${fileName.replace(/\.[jt]s$/, "")}';
 
-useMigrations<m_${version}.Schema>('${db}', {
+useMigrations${language === "ts" ? `<m_${version}.Schema>` : ""}('${db}', {
 ${existing}
   [m_${version}.version]: m_${version}.migration,
 });
